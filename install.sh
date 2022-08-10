d446f45cf4b6b903ff74772fa04e8f999791ad18
François Michonneau <francois.michonneau@gmail.com>
2022-08-10 12:00:25 +0200

    make path more general

--- install.sh
+++ install.sh
@@ -3,10 +3,11 @@
 stow emacs
 stow git
 stow R
+stow local
 stow config
 stow bash
 stow ccache
 
-printf "## don't edit this file by hand, edit the install script instead\n38 6,9,12 * * * bash /home/francois/.R/update_library.sh >> /tmp/update_r_library.log 2>&1\n" > ~/etc/crontab
+printf "## don't edit this file by hand, edit the install script instead\n38 6,9,12 * * * bash %s/.R/update_library.sh >> /tmp/update_r_library.log 2>&1\n" "$HOME" > ~/etc/crontab
 
 crontab ~/etc/crontab
