f62ead1e45d24a717279cf3ff50f59bb67eca3f8
François Michonneau <francois.michonneau@gmail.com>
2022-08-10 11:56:20 +0200

    generalize home paths

--- R/.R/update_library.sh
+++ R/.R/update_library.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo "$(date) starting to update R library" &&
-    /usr/bin//Rscript -e 'readRenviron("/home/francois/.Renviron"); .libPaths("/home/francois/.R/library"); source("/home/francois/.R/auto_update.R"); auto_update()' &&
+    /usr/bin//Rscript -e 'readRenviron(file.path(Sys.getenv("HOME"), ".Renviron")); .libPaths(file.path(Sys.getenv("HOME"), ".R/library")); source(file.path(Sys.getenv("HOME"), ".R/auto_update.R")); auto_update()' &&
     echo "   ... DONE!"
