983b65267c942640fa7157c9977e9ddaea96b272
François Michonneau <francois.michonneau@gmail.com>
2025-04-04 15:59:54 +0200

    temp fix

--- install.sh
+++ install.sh
@@ -3,14 +3,15 @@
 mkdir -p ~/etc
 mkdir -p ~/.R-library
 
-stow emacs
-stow git
-stow R
-stow local
-stow config
-stow bash
-stow ccache
+stow emacs -t $HOME -v
+stow git  -t $HOME -v
+stow R  -t $HOME -v
+#stow local  -t $HOME -v
+stow config  -t $HOME -v
+# stow bash  -t $HOME -v
+stow ccache  -t $HOME -v
 
-printf "## don't edit this file by hand, edit the install script instead\n38 6,9,12 * * * bash %s/.R/update_library.sh >> /tmp/update_r_library.log 2>&1\n" "$HOME" > ~/etc/crontab
 
-crontab ~/etc/crontab
+# printf "## don't edit this file by hand, edit the install script instead\n38 6,9,12 * * * bash %s/.R/update_library.sh >> /tmp/update_r_library.log 2>&1\n" "$HOME" > ~/etc/crontab
+
+# crontab ~/etc/crontab
